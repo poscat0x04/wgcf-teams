f435079c3d1f82fb372ad22e3740207a83636ff9
poscat <poscat@poscat.moe>
2023-11-18 12:22:50 +0800

    update flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:poscat0x04/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
   };
@@ -21,9 +21,11 @@
       };
     }) // {
       overlay = final: prev: {
-        wgcf-teams = with final.rustPlatform; buildRustPackage {
-          pname = "wgcf-teams";
-          version = "0.1.0";
+        wgcf-teams = let
+          cargo-toml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        in with final.rustPlatform; buildRustPackage {
+          pname = cargo-toml.package.name;
+          version = cargo-toml.package.version;
 
           src = nix-filter.lib {
             root = ./.;
